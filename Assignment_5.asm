765db70e8fc68fbc7dd6619368c7ce72628b9e12
Tri Doan <tridoan1998@gmail.com>
2018-03-01 17:45:46 -0800

    Add files via upload

--- Assignment_5.asm
+++ Assignment_5.asm
@@ -1,11 +1,9 @@
 ;Tri Doan / 03/01/2018 
 comment ! 
-A prime number is an integer that is divisible only by 1 and by itself. Write a program that:
-
+Objective:A prime number is an integer that is divisible only by 1 and by itself. Write a program that:
 Takes a number as an input.
 Prints back to the console all the primes that are larger than 1 but not larger than the number that has been entered.
 You need to have a procedure called isPrime. You may have more procedures if you like.
-
 This procedure receives a number through one of the registers.
 Checks if this number is prime.
 Returns back through one of the registers to the main program 1, if the number is prime, 0 otherwise.
@@ -18,62 +16,72 @@ include Irvine32.inc
 ;create strings 
 enter_string BYTE "Please enter a number: ", 0	
 display_result_string BYTE "Primes found until the given number: ", 0
+string BYTE "Primes found until the given number: ", 0
+space BYTE " ", 0
 
 ;create variables 
-input DWORD	 ?			;keep user input
-count DWORD  2			;hold eax, the input
-keep_eax   DWORD  ?     ;keep eax 
+input DWORD			?			;keep user input
+count DWORD			2			;hold eax, the input
+
 .code
 main proc
 	mov edx, offset enter_string	;move string to edx
 	call writeString				;cout string
-
 	call readDec					;input number, store in eax
-
-	mov keep_eax, eax 
-	inc count 
-	mov eax, count 
-	call writeDec
-	call isPrime					;call isPrime proceture
-									;the resule store in ebx
-									;1 if prime, 0 if not
 	
-	mov eax, keep_eax				;put input back to eax
 
-	;this while loop print the numbers <= the input that are prime
-	while2:								;check count and input
-		cmp count, eax					;compare count and input
-		ja outwhile2					;jump out if count above input
+	mov edx, offset string			;move string into edx
+	call writeString				;cout string
+
+	call Crlf 
+	mov esi, eax					;esi hold the input 
+	mov edx, offset space			;move string to edx 
+	mov ecx, count					;move count to ecx
+	
+	;this while2 loop check count varible if it <= input 
+	;while count is <= and it's prime, we print out the count 
+	;if not, we increate count and continue the loop
+	while2:
+		cmp ecx, esi				;move esi, the input, into ecx
+		jae outwhile2				;jump out if ecx above equal to esi
+		mov eax, ecx				;
+		call isPrime					;call isPrime proceture
 		cmp ebx, 1
-		jne else1
-		mov eax, ecx 
+		jne out3
+		mov eax, ecx
 		call writeDec
-		inc ecx						;increate count
+		call writeString
+		out3:
+		inc ecx
 
 		jmp while2
-	outwhile2:
 
-
-	else1:
-
-
-
+		outwhile2:
+ 
 	exit
 main endp
 
+;isPrime take in the  input, eax, and then check 
+;if the input is prime, return ebx = 1
+;if not, return ebx = 0
 isPrime PROC
-	mov ebx, eax			;hold count  
-	mov ecx, 2				;hold temp
+push ecx					;keep ecx value in the stack
+push edx					;keep edx value in the stack
 
+	mov ebx, eax			;hold input  
+	mov ecx, 2				;hold temp(counter)
+
+	;while1 loop to check the input with counter ecx = 2
+	;perform the division eax/ecx 
 	while1:
-		cmp ecx, ebx
-		jae outwhile1
+		cmp ecx, ebx		;compare counter with input
+		jae outwhile1		;jump out if above or equal 
 	
-		mov eax, ebx
+		mov eax, ebx		;
 		xor edx, edx		;make edx 0
 		div	ecx				;edx:eax/ecx
 		cmp edx, 0
-		jz notPrime			;jump out when 0
+		jz notPrime			;jump out to notPrime if remainder is 0
 		inc ecx				;increate temp
 		jmp while1			;jump back to the top
 	outwhile1:
@@ -83,8 +91,8 @@ isPrime PROC
 		mov ebx, 0			;make ebx 0 if not prime 
 	out1:
 
-
-
+pop edx
+pop ecx
 
 	ret 
 isPrime ENDP
@@ -100,10 +108,9 @@ comment
 sample run:
 
 Please enter a number: 35
-
 Primes found until the given number:
-
 2 3 5 7 11 13 17 19 23 29 31 Press any key to continue . . .
+
 !
 
 
