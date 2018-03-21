da6042290b997faf920339d1da7e7c72fa957a54
Tri Doan <tridoan1998@gmail.com>
2018-03-21 16:33:56 -0700

    Add files via upload

--- main.asm
+++ main.asm
@@ -29,17 +29,22 @@ string3 BYTE "Enter the type of your array.", 0
 string4 BYTE " 1 for byte.", 0
 string5 BYTE " 2 for word.", 0
 string6 BYTE " 4 for doubleword.", 0
-string7 BYTE "Enter an element in your array, ", 0
+string7 BYTE "Enter an element in your array: ", 0
 string8 BYTE "Enter row number that you want me to sum: ", 0
 string9 BYTE "The sum is: ", 0
+string10 BYTE "Wrong input. Please run the program again", 0
+ 
+;create arrays with different size
+maxElement = 40
+arrayB byte maxElement dup (0)
+arrayW word maxElement dup(0)
+arrayD dword maxElement dup(0)
 
-;create avaiables
-
-Array 
-
+;create variables to keep user input
 ArrayElement dword  ?			;keep how many elements from user input 
 ArrayRow     dword  ?			;keep how many row from user input
 ArrayType    dword  ?			;keep what type	from user input
+ArrayRowIndex	dword ?			;keep row index
 
 
 .code	
@@ -48,7 +53,7 @@ main proc
 ;make an if else loop to check if maxSize is >= 40
 mov edx, offset string1				;move affress string1 into edx
 call writeString					;call string1
-call readInt						;read input (keep how many elements)
+call readDec						;read input (keep how many elements)
 mov ArrayElement, eax				;put input into ArrayElement
 cmp eax, 1							;compare eax with 1
 jb else1							;jump if below to else1 
@@ -63,7 +68,7 @@ else1:								;else1 label
 out1:								;within out1 we will make the program
 	mov edx, offset string2				;move address string3 into edx
 	call writeString					;call string
-	call readInt						;read in input
+	call readDec						;read in input
 	mov ArrayRow, eax					;move ArrayRow = eax
 	mov edx, offset string3				;move address string3 to edx
 	call writeString					;call string
@@ -74,19 +79,81 @@ out1:								;within out1 we will make the program
 	mov edx, offset string5				;move address string5 to edx
 	call writeString					;call string
 	call Crlf							;go down one line
-	mov ArrayType, eax					;move ArrayType = eax
 	mov edx, offset string6				;move address string6 to edx
 	call writeString					;call string
 	call Crlf							;go down one line
+	call readDec						;input the type
+	mov ArrayType, eax					;move ArrayType = eax
 
-mov ecx, ArrayElement
-l1: 
-	string7
-	Loop l1
+
+	;now we perform the switch statement to check which array to use
+	cmp ArrayType, 1					;check array type with 1
+	JE case1							;jump to case3 if equal 
+	cmp ArrayType, 2					;check array type with 2
+	JE case2							;jump to case3 if equal 
+	cmp ArrayType, 4					;check array type with 4
+	JE case3							;jump to case3 if equal 
+	jmp else2							;jump to else2 label 
+	case1:								;case1 label
+		mov ecx, ArrayElement			;loop counter
+		mov esi, offset arrayB			;make esi points to arrayB
+		;the loop uses to input elements for array
+		l1:
+			mov edx, offset string7		;put string7 into edx
+			call writeString			;call string
+			call readDec				;user input
+			mov [esi], eax				;move element into where esi point at
+			add esi, TYPE arrayB		;move to the next array element
+			Loop l1
+		jmp out2
+	case2:
+			mov ecx, ArrayElement			;loop counter
+		mov esi, offset arrayW			;make esi points to arrayB
+		;the loop uses to input elements for array
+		l2:
+			mov edx, offset string7		;put string7 into edx
+			call writeString			;call string
+			call readDec				;user input
+			mov [esi], eax				;move element into where esi point at
+			add esi, TYPE arrayW		;move to the next array element
+			Loop l2
+		jmp out2
+	case3:
+			mov ecx, ArrayElement			;loop counter
+		mov esi, offset arrayD			;make esi points to arrayB
+		;the loop uses to input elements for array
+		l3:
+			mov edx, offset string7		;put string7 into edx
+			call writeString			;call string
+			call readDec				;user input
+			mov [esi], eax				;move element into where esi point at
+			add esi, TYPE arrayD		;move to the next array element
+			Loop l3
+		jmp out2
+else2:									;else2 label
+	mov edx, offset string10			;move string10 to edx
+	call writeString					;call string
+	call Crlf							;go down one line
+	exit								;exit
+out2:									;out2 label
+
+
+mov edx, offset string8				;set string to edx
+call writeString					;call string
+call readInt						;read user input
+mov ArrayRowIndex, eax				;keep input in ArrayRowIndex
+mov eax, ArrayElement				;move ArrayElement to eax
+mov ebx, ArrayRow					;move ArrayRow to ebx
+mov ecx, ArrayType					;move ArrayType to ecx
+mov edx, ArrayRowIndex				;move ArrayRowIndex to edx
+call calcRowSum						;call procesure
+
+
 
 exit
 main endp
-end main
+
+
 
 ;calculates the sum of one row in the two-dimensional array of bytes, words,
 ;or doublewords and returns it to main proc.
@@ -95,9 +162,10 @@ end main
 calcRowSum PROC
 
 
-	ret 
-calcRowSum endp 
+calcRowSum ENDP 
 
+
+end main
 
 
 comment !
