f030f92f59c65d72cd98c2c09f86697e1eb29964
Tri Doan <tridoan1998@gmail.com>
2018-03-22 19:40:25 -0700

    Add files via upload

--- main.asm
+++ main.asm
@@ -47,15 +47,14 @@ arrayD dword maxElement dup(0)
 
 
 ;create variables to keep user input
-ArrayElement dword  ?			;keep how many elements from user input 
-ArrayRow     dword  ?			;keep how many row from user input
-ArrayType    dword  ?			;keep what type	from user input
-ArrayRowIndex	dword ?			;keep row index
+ArrayElement	 dword  ?			;keep how many elements from user input 
+ArrayRow		 dword  ?			;keep how many row from user input
+ArrayType		 dword  ?			;keep what type	from user input
+ArrayRowIndex	 dword  ?			;keep row index
 
 
 .code	
 main proc
-sub esp, 4
 ;make an if else loop to check if maxSize is >= 40
 mov edx, offset string1				;move affress string1 into edx
 call writeString					;call string1
@@ -91,7 +90,6 @@ out1:								;within out1 we will make the program
 	call readDec						;input the type
 	mov ArrayType, eax					;move ArrayType = eax
 
-
 	;now we perform the switch statement to check which array to use
 	cmp ArrayType, 1					;check array type with 1
 	JE case1							;jump to case3 if equal 
@@ -112,20 +110,11 @@ out1:								;within out1 we will make the program
 			add esi, TYPE arrayB		;move to the next array element
 			Loop l1
 
-
-		mov ecx, ArrayElement
-		mov esi, offset arrayB
-		l4:
-			movzx eax, BYTE ptr [esi]
-			call writeDec
-			add esi, TYPE arrayB
-			call Crlf
-			Loop l4
-			
+		sub esp, 4						;make room for the returning sum 
 		push offset arrayB				;make esi points to arrayB
-		jmp out2
-	case2:
-			mov ecx, ArrayElement			;loop counter
+		jmp out2						;jump to out2 label
+	case2:								;run this if user enter 2 
+		mov ecx, ArrayElement			;loop counter
 		mov esi, offset arrayW				;make esi points to arrayB
 		;the loop uses to input elements for array
 		l2:
@@ -135,10 +124,11 @@ out1:								;within out1 we will make the program
 			mov [esi], eax				;move element into where esi point at
 			add esi, TYPE arrayW		;move to the next array element
 			Loop l2
+			sub esp, 4
 			push offset arrayW			;make esi point to arrayW
-		jmp out2
-	case3:
-			mov ecx, ArrayElement			;loop counter
+		jmp out2						;jump to out2 label
+	case3:								;run if user input 4
+		mov ecx, ArrayElement			;loop counter
 		mov esi, offset arrayD			;make esi points to arrayB
 		;the loop uses to input elements for array
 		l3:
@@ -148,7 +138,8 @@ out1:								;within out1 we will make the program
 			mov [esi], eax				;move element into where esi point at
 			add esi, TYPE arrayD		;move to the next array element
 			Loop l3
-			push offset arrayD			;make esi points to arrayD
+			sub esp, 4					; make room for the returning sum 
+			push offset arrayD			; pass address of array to the stack 
 		jmp out2
 else2:									;else2 label
 	mov edx, offset string10			;move string10 to edx
@@ -162,18 +153,23 @@ call writeString					;call string
 call readInt						;read user input
 mov ArrayRowIndex, eax				;keep input in ArrayRowIndex
 
-push ArrayRow						;push into stack
-push ArrayType						;push into stack
-push ArrayRowIndex					;push i
+
+;int calcRowSum ( int *array, int rowSize, int type, int rowIndex);
+
+
+push ArrayRowIndex
+push ArrayType
+push ArrayRow
+call calcRowSum
+pop eax
+call writeHex
 
 
 
-mov eax, ArrayElement				;move ArrayElement to eax
-mov ebx, ArrayRow					;move ArrayRow to ebx
-mov ecx, ArrayType					;move ArrayType to ecx
-mov edx, ArrayRowIndex				;move ArrayRowIndex to edx
 
-call calcRowSum						;call procesure
+
+
+
 
 
 
@@ -197,11 +193,67 @@ calcRowSum PROC
 	push edi
 
 
+	mov eax, [ebp+8]		;hold array row
+	mov ecx, [ebp+12]		;hold array type
+	xor edx, edx			;make edx = 0
+	mul ecx				;multiplication of row and type, store result in eax
 
+	xor ecx, ecx			;make ecx = 0 
+	mov ecx, [ebp+16]		;point ecx to rowindex
 
+	mul ecx					;multiplication of eax and rowarray, store in eax
 
+	
+	mov ebx, [ebp+20]		;ebx keep value inside ebp+20 
+	add ebx, eax			;row offset
+	mov eax, 0				;accumulator
+	mov esi, 0				;column index
 
-	ret 
+	mov edi, [ebp+12]
+	cmp edi, 1
+	je case1
+	cmp edi, 2 
+	je case2
+	cmp edi, 4
+	je case3
+	jmp out1
+	case1:
+		mov ecx, [ebp+8]
+		L11:
+			movzx edx, BYTE PTR [ebx + esi] 
+			add eax, edx
+			inc esi
+			loop L11
+			mov [ebp+24], eax	;set eax to where ebp point at
+			jmp out1
+	case2:
+		mov ecx, [ebp+8]
+		L12:
+			movzx edx, WORD PTR [ebx + esi] 
+			add eax, edx
+			add esi, 2
+			loop L12
+			mov [ebp+24], eax	;set eax to where ebp point at
+			jmp out1
+	case3:
+		mov ecx, [ebp+8]
+		L13:
+			mov edx, [ebx + esi] 
+			add eax, edx
+			add esi, 4
+			loop L13
+			mov [ebp+24], eax	;set eax to where ebp point at
+			jmp out1
+	out1:
+	
+	pop edi 
+	pop esi 
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	pop ebp
+	ret 12 
 calcRowSum ENDP 
 
 
@@ -230,5 +282,12 @@ Press any key to continue . . .
 
 
 
-
+	mov ecx, ArrayElement
+		mov esi, offset arrayB
+		l4:
+			movzx eax, BYTE ptr [esi]
+			call writeDec
+			add esi, TYPE arrayB
+			call Crlf
+			Loop l4
 !
